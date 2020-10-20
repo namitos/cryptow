a5b9ab7f66aca8b0372af30dacb56188396e5275
namitos <namit@yandex.ru>
2020-10-20 15:48:40 +0300

    initial commit

--- cryptow.go
+++ cryptow.go
@@ -2,9 +2,22 @@ package cryptow
 
 import (
 	"crypto/rand"
+	"crypto/sha256"
+	"crypto/sha512"
 	"encoding/base32"
 	"encoding/base64"
+	"encoding/hex"
 )
+
+func Sha256String(in string) string {
+	sum := sha256.Sum256([]byte(in))
+	return hex.EncodeToString(sum[:])
+}
+
+func Sha512String(in string) string {
+	sum := sha512.Sum512([]byte(in))
+	return hex.EncodeToString(sum[:])
+}
 
 func RandBytesString(size, base int) (string, error) {
 	token := make([]byte, size)
